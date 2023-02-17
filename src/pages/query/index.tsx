8d0972f29ab97f755f650acd685988774725fdc3
李涛 <litaowork@aliyun.com>
2023-02-17 21:16:22 +0800

    feat: show URL pathname if there be

--- src/pages/query/index.tsx
+++ src/pages/query/index.tsx
@@ -6,11 +6,9 @@ import "antd/es/message/style/index";
 
 import { useValue } from "@/hooks";
 
-const default_value =
-  "https://ccp-bj29-video-preview.oss-enet.aliyuncs.com/lt/4A83485078C8F202915703D840D30C97251C76D5_420511000__sha1_bj29/SD/media.m3u8?di=bj29&dr=622310670&f=63db451b5d1a9c133396487eb61bbcd9becd0f84&security-token=CAIS%2BgF1q6Ft5B2yfSjIr5eGDuj1gppm4LbdQVb8lXEzWLdD2K7qrDz2IHFPeHJrBeAYt%2FoxmW1X5vwSlq5rR4QAXlDfNR27eVbZqVHPWZHInuDox55m4cTXNAr%2BIhr%2F29CoEIedZdjBe%2FCrRknZnytou9XTfimjWFrXWv%2Fgy%2BQQDLItUxK%2FcCBNCfpPOwJms7V6D3bKMuu3OROY6Qi5TmgQ41Uh1jgjtPzkkpfFtkGF1GeXkLFF%2B97DRbG%2FdNRpMZtFVNO44fd7bKKp0lQLukMWr%2Fwq3PIdp2ma447NWQlLnzyCMvvJ9OVDFyN0aKEnH7J%2Bq%2FzxhTPrMnpkSlacGoABj65Y5%2FGAWAC9ag7QcryQ5gkSCTt9N9Nps4GXoVM1u8Am4Bxzn9UTft7vXq07LpmwLYLSAX9meWl52D9ea4xACVZOXGfLXA0jR7i0bvOpylbNtvkCC7WcjGHpE0ZlUR17PtxThElElZsB%2FndIHjs9%2Fkmx0nfmpAttq0uyCrO7dr0%3D&u=5565045eef84445cbdece790aebe54bd&x-oss-access-key-id=STS.NT3ERAoEGWt6CpMqqfT8o7kAN&x-oss-expires=1676623373&x-oss-process=hls%2Fsign&x-oss-signature=RDb%2BM2eaaADLbVrA2xqVwcF2e0%2FpVaJIRvuu0SFGSOk%3D&x-oss-signature-version=OSS2";
-
 const URLQueryParserPage = () => {
-  const [url, setUrl] = useValue<string>(default_value);
+  const [url, setUrl] = useValue<string>("");
+  const [pathname, setPathname] = useState("");
   const [keyArr, setKeyArr] = useState<
     {
       key: string;
@@ -27,6 +25,7 @@ const URLQueryParserPage = () => {
             className="w-full h-24 input"
             placeholder="请输入 url"
             value={url}
+            rows={8}
             onChange={setUrl}
           />
           <button
@@ -37,7 +36,9 @@ const URLQueryParserPage = () => {
               }
               let search = decodeURIComponent(url);
               if (url.includes("?")) {
-                search = url.split("?")[1];
+                const [path, searchStr] = url.split("?");
+                search = searchStr;
+                setPathname(path);
               }
               const result = qs.parse(search, { ignoreQueryPrefix: false });
               const values = Object.keys(result).map((k) => {
@@ -52,6 +53,7 @@ const URLQueryParserPage = () => {
             解析
           </button>
         </div>
+        {pathname && <div className="mt-8 text-2xl break-all">{pathname}</div>}
         <div className="mt-8 space-y-4">
           {keyArr.map(({ key, value }) => {
             return (
